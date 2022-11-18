cb3037249be25451d919d87e97c3d46aceac982a
KJ Monahan <kj@forumone.com>
2022-11-18 16:38:11 -0600

    [SLACDP22-188] Person detail template and demo page

--- source/04-templates/people-profile/people-profile.stories.jsx
+++ source/04-templates/people-profile/people-profile.stories.jsx
@@ -1,24 +1,36 @@
 import parse from 'html-react-parser';
+import ReactDOMServer from 'react-dom/server';
+import React from 'react';
 
 import twigTemplate from './people-profile.twig';
 import globalData from '../../00-config/storybook.global-data.yml';
 import data from './people-profile.yml';
+import './people-profile.scss';
+import { WysiwygWrapper } from '../../06-utility/storybookHelper.jsx';
 
 const settings = {
-  title: 'Templates/Page',
+  title: 'Templates/People Profile',
   parameters: {
     controls: {
       include: [
-        'title',
         'show_admin_info',
-        'show_footer',
-        'author_name',
-        'date_format',
-        'year',
-        'month',
-        'day',
-        'hour',
-        'minute',
+        'bio_name',
+        'bio_job_title',
+        'bio_organization',
+        'bio_image',
+        'bio_page_content',
+        'bio_email',
+        'bio_phone',
+        'bio_mobile',
+        'bio_fax',
+        'bio_location_name',
+        'bio_location_address',
+        'bio_map',
+        'bio_interestareas',
+        'bio_education',
+        'bio_awardshonors',
+        'bio_affiliations',
+        'bio_links',
       ],
     },
   },
@@ -28,6 +40,9 @@ const PeopleProfile = args => parse(twigTemplate(args));
 PeopleProfile.args = {
   ...globalData,
   ...data,
+  bio_page_content: ReactDOMServer.renderToStaticMarkup(
+    <WysiwygWrapper>{parse(data.bio_page_content)}</WysiwygWrapper>
+  ),
 };
 
 export default settings;
