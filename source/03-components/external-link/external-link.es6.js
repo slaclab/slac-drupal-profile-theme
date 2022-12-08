88f54d689cefbcd87693a78d73390fc3d727a6c3
KJ Monahan <kj@forumone.com>
2022-12-08 14:28:17 -0600

    [SLACDP22-121] Integrate event detail page

--- source/03-components/external-link/external-link.es6.js
+++ source/03-components/external-link/external-link.es6.js
@@ -52,7 +52,7 @@ Drupal.behaviors.externalLinks = {
     }
 
     const externalLinks = context.querySelectorAll(
-      "a:not([href=''], [href^='#'], [href^='?'], [href^='/'], [href^='.'], [href^='javascript:'], [href^='mailto:'], [href^='tel:'], .c-logo, .c-social-links__link, .rss-link)"
+      "a:not([href=''], [href^='#'], [href^='?'], [href^='/'], [href^='.'], [href^='javascript:'], [href^='mailto:'], [href^='tel:'], .c-logo, .c-social-links__link, .rss-link, .c-event-details__link)"
     );
 
     externalLinks.forEach(el => {
